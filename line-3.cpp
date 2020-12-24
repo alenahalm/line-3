e4fe959def2373f1d1a7e0752d21fd6491ca5952
alenahalm <alenahalmatova749@gmail.com>
2020-12-24 14:17:28 +0800

    Initial commit

---
+++ line-3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double k, p, sum;
+	int n;
+	cout << "Enter capital" << endl;
+	cin >> k;
+	cout << "Enter percentage" << endl;
+	cin >> p;
+	p = p / 100.0;
+	cout << "Enter the sum" << endl;
+	cin >> sum;
+
+
+	// double x = 0.5;
+	// s = log10(x);
+	n = ceil(log((double)(sum / k)) / log((double)(1.0 + p)));
+	cout << "Years: " << n << endl;
+	system("pause");
+}
